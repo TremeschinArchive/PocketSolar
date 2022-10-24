92517f8e6e6cbdec4fa2014b3705a73980aa2bd6
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2022-10-24 00:21:13 -0400

    Almost all code logistics solved

--- ViyLine.rs
+++ ViyLine.rs
@@ -1,7 +1,136 @@
 // | (c) 2022 Tremeschin, MIT License | ViyLine Project | //
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(non_snake_case)]
 use Protostar::*;
 
+// Define a point on the IV Curve
+struct PointIV {
+    v: f64,
+    i: f64,
+}
+
+struct CurveIV {
+    points: Vec<PointIV>,
+}
+
+impl CurveIV {}
+
+struct ViyLineApp {
+    // Example stuff:
+    label: String,
+
+    // this how you opt-out of serialization of a member
+    value: f32,
+}
+
+impl Default for ViyLineApp {
+    fn default() -> Self {
+        Self {
+            label: "Hello World!".to_owned(),
+            value: 2.7,
+        }
+    }
+}
+
+impl eframe::App for ViyLineApp {
+    /// Called by the frame work to save state before shutdown.
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self { label, value } = self;
+
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        _frame.close();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Side Panel");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(label);
+            });
+
+            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                *value += 1.0;
+            }
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label("powered by ");
+                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label(" and ");
+                    ui.hyperlink_to(
+                        "eframe",
+                        "https://github.com/emilk/egui/tree/master/crates/eframe",
+                    );
+                    ui.label(".");
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            ui.heading("eframe template");
+            ui.hyperlink("https://github.com/emilk/eframe_template");
+            ui.add(egui::github_link_file!(
+                "https://github.com/emilk/eframe_template/blob/master/",
+                "Source code."
+            ));
+            egui::warn_if_debug_build(ui);
+        });
+
+        if false {
+            egui::Window::new("Window").show(ctx, |ui| {
+                ui.label("Windows can be moved by dragging them.");
+                ui.label("They are automatically sized based on contents.");
+                ui.label("You can turn on resizing and scrolling if you like.");
+                ui.label("You would normally chose either panels OR windows.");
+            });
+        }
+    }
+}
+
+use eframe::egui;
+
 fn main() {
     println!("Hello ViyLine");
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        let web_options = eframe::WebOptions::default();
+        eframe::start_web(
+            "the_canvas_id",
+            web_options,
+            Box::new(|_cc| Box::new(ViyLineApp::default())),
+        )
+        .expect("failed to start eframe");
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let options = eframe::NativeOptions::default();
+        eframe::run_native(
+            "My egui App",
+            options,
+            Box::new(|_cc| Box::new(ViyLineApp::default())),
+        );
+    }
 }
